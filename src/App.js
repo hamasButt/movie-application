316a1921ba91816fac840e53fa053366aa7ad2e9
Hamas Ahmed <hamasahmed341@gmail.com>
2022-05-19 15:17:51 +0500

    Code Cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import {
+    BrowserRouter as Router,
+} from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Routing from "./common/Routing/Routing";
+
+
 const App = () => {
 
 
-  return (
-    <div className='app'>
-      hello
-    </div>
-  )
+    return (
+        <div className='app'>
+            <Router>
+                <Header/>
+                <div className="container">
+                    <Routing/>
+                </div>
+                <Footer/>
+            </Router>
+        </div>
+    )
 }
 
 export default App
